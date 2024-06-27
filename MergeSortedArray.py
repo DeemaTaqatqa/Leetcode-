01a7bd766005f39fcc0c9fcfa443634ba28ab29a
Dima Taqatqa <88555807+DimaTaqatqa@users.noreply.github.com>
2024-06-27 22:50:01 +0300

    solving Merge Sorted Array

---
+++ MergeSortedArray.py
@@ -0,0 +1,39 @@
+def merge(nums1, nums2, m,n):
+    nums1cp= nums1[:m]
+    p1=0
+    p2=0
+    for p in range(n+m):
+        if p2>=n or (p1 < m and nums1cp[p1]<=nums2[p2]):
+            nums1[p]=nums1cp[p1]
+            p1+=1
+        else:
+            nums1[p]=nums2[p2]
+            p2+=1
+        
+#here O(n+m) and space O(n) both from sorting function
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+merge(nums1, nums2, m,n)
+print(nums1)
+#because of sort runtime O(n+mlog(m+n)) and space O(n)
+# def merge(nums1, nums2, m,n):
+#     for i in range(n):
+#                 nums1[m+i] = nums2[i]
+#             nums1.sort()
+
+# O(n+m) and O(1) no extra space
+            # p2= n-1
+            # p1= m-1
+    
+            # for p in range(n+m-1, -1, -1):
+            #     if p2 <0 :
+            #         break
+            #     if p1 >= 0  and nums1[p1] > nums2[p2]:
+            #         nums1[p]=nums1[p1]
+            #         p1-=1
+            #     else:
+            #         nums1[p]=nums2[p2]
+            #         p2-=1
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ This is a repo that has all my solved questions on leetcode website.
 - [Valid Anagram](https://leetcode.com/submissions/detail/1222169904/)
 - [Two Sum](https://leetcode.com/submissions/detail/1228081725/)
 - [DuplicateZeros](https://leetcode.com/submissions/detail/1250405809/)
+- [Merge Sorted Array](https://leetcode.com/submissions/detail/1302184322/)
