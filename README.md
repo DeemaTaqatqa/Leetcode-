5b503d6d2fe0ac79b0382f779887a4b933653499
Dima Taqatqa <88555807+DimaTaqatqa@users.noreply.github.com>
2024-09-16 19:43:22 +0300

    solve Valid Anagram problem

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a repo that has all my solved questions on leetcode website.
 ### These are the problems that I have solved so far:
 
 - [Contains Duplicate](https://leetcode.com/submissions/detail/1222134269/)
-- [Valid Anagram](https://leetcode.com/submissions/detail/1222169904/)
+- [Valid Anagram](https://leetcode.com/submissions/detail/1392308058/)
 - [Two Sum](https://leetcode.com/submissions/detail/1228081725/)
 - [Duplicate Zeros](https://leetcode.com/submissions/detail/1250405809/)
 - [Merge Sorted Array](https://leetcode.com/submissions/detail/1302184322/)
--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -1,22 +1,22 @@
 def isAnagram(s, t):
-    s_list=  [char for char in s]
-    t_list =  [char for char in t]
-    t_list.sort()
-    s_list.sort()
-    return t_list == s_list
-# alternative answer
-# hashS, hashT ={}, {} # two hash table
-# if len(hashS) != hashT: if the length is not equal 
-#     return False
+    hashS, hashT ={}, {} # two hash table
+    if len(s) != len(t): #if the length is not equal 
+        return False
 
-# for i in range(len(s)):
-#     hashS[s[i]]= 1+ hashS.get(s[i],0) the hash key is the letter and the value is the frequancy 
-#     hashT[t[i]]= 1+ hashS.get(t[i],0)
+    for i in range(len(s)): # the two strings have the same length so iterate through one doesn't matter
+        hashS[s[i]]= 1+ hashS.get(s[i],0) #the hash key is the letter and the value is the frequancy 
+        # the point of using get to not thorugh an error if the key doesn't exist 
+        # the line is c = 1 + c
+        hashT[t[i]]= 1+ hashS.get(t[i],0)
 
-# for c in hashS:
-#     if hashS[c]!= hashT[c]: if each letter frequancy is not equal 
-#         return False
-# return True
+    for c in hashS:
+        if hashS[c]!= hashT.get(c,0): #if each letter frequancy is not equal 
+            return False
+    return True
+
+# the other way to make this is counter(s) == counter(t) make dictonary with counters of the frequancy
+
 # the problem here is the time complextity is iterating in s and t O(s+t) the same for space because it makes 2 tabels with size s+t
+
 # if want to make the space O(1) (usually O(nlogn)) considering sort funciton has no space
 #return sorted(s)==sorted(t) # no extra space
