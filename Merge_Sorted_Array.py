40402b011dc7c7627bba6194432dc47436cd8dea
Dima Taqatqa <88555807+DimaTaqatqa@users.noreply.github.com>
2025-01-03 17:40:03 +0200

    Merge Sorted Array is updated

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -1,14 +1,16 @@
 def merge(nums1, nums2, m,n):
     nums1cp= nums1[:m]
-    p1=0
-    p2=0
-    for p in range(n+m):
-        if p2>=n or (p1 < m and nums1cp[p1]<=nums2[p2]):
-            nums1[p]=nums1cp[p1]
-            p1+=1
+    p1= m -1
+    p2= n - 1
+    for p in range(n+m-1, -1, -1):
+        if p2 < 0:
+            break
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p]=nums1[p1]
+            p1-=1
         else:
             nums1[p]=nums2[p2]
-            p2+=1
+            p2-=1
         
 #here O(n+m) and space O(n) both from sorting function
 
