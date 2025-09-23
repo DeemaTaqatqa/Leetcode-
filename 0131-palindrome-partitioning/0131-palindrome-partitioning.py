bd6473e8dc5cd4954131335c0a484c468233fe73
Deema Taqatqa <88555807+DeemaTaqatqa@users.noreply.github.com>
2025-09-23 20:42:38 +0300

    Time: 43 ms (83.32%), Space: 34.6 MB (46.46%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,23 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        for l in range(1, n + 1):
-            for i in range(n - l + 1):
-                dp[i][i + l - 1] = (s[i] == s[i + l - 1] and
-                                    (i + 1 > (i + l - 2) or
-                                    dp[i + 1][i + l - 2]))
-
-        res, part = [], []
-        def dfs(i):
-            if i >= len(s):
-                res.append(part.copy())
+        res = []
+        
+        def backtrack(start, subset):
+            if start == len(s):
+                res.append(subset[:])
                 return
-            for j in range(i, len(s)):
-                if dp[i][j]:
-                    part.append(s[i : j + 1])
-                    dfs(j + 1)
-                    part.pop()
 
-        dfs(0)
+            for i in range( start, len(s)):
+                if isPal(s, start, i):
+                    subset.append(s[start:i+1])
+                    backtrack(i + 1, subset)
+                    subset.pop()
+    
+        def isPal(s, l, r):
+            while l < r :
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        backtrack(0, [])
         return res
